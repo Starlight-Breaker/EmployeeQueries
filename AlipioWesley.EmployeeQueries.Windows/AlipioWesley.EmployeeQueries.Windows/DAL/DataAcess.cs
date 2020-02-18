72e2518dd57c93ee642479f36d0b44f76785734f
Jan Wesley Alipio <alipiojanwesley@gmail.com>
2020-02-18 16:39:52 +0800

    >Installed Packages >DAL >App Configuration

---
+++ AlipioWesley.EmployeeQueries.Windows/AlipioWesley.EmployeeQueries.Windows/DAL/DataAcess.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+
+namespace AlipioWesley.EmployeeQueries.Windows.DAL
+{
+    public class DataAcess : DbContext
+    {
+        public DataAcess() : base("myConnectionString")
+        {}
+    }
+}
